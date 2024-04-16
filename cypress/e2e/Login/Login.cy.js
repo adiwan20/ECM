455f334f9131092cfd68db0fe2d812b0973fab4c
Archita Diwan <adiwan@sst.saisystems.com>
2024-04-16 15:18:59 +0530

    initial script setup

--- cypress/e2e/Login/Login.cy.js
+++ cypress/e2e/Login/Login.cy.js
@@ -3,6 +3,9 @@ describe("login scenario1",() => {
     it("Verify successful login", () =>{
     
     cy.visit("https://pmsdevui.azurewebsites.net/")
+    cy.get('input[name="email"]').type('adiwan@health.saisystems.com')
+    cy.get('input[name="key"]').type('Abcde@12345')
+    cy.get('button[type="submit"]').click();
     })
       
     })
